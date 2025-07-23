3c5a390261c86b1f8c53c7d9a93b9c389c54f40f
pfbahury <90939515+pfbahury@users.noreply.github.com>
2025-07-23 20:21:46 -0300

    feat: add post route

--- routes/boletos.py
+++ routes/boletos.py
@@ -1,10 +1,21 @@
 from fastapi import APIRouter, HTTPException
 from db.db import get_db_connection
 from psycopg import Error, sql
+from pydantic import BaseModel
+
+class Boleto(BaseModel):
+    numero_boleto: str
+    data_emissao: str
+    data_vencimento: str
+    valor: float
+    status: str
+    cliente: str
+    empresa: str
+
 
 router = APIRouter()
 
-@router.get("/boletos/")
+@router.get("/boletos")
 def get_boletos():
     conn = get_db_connection() 
     if conn is None:
@@ -21,6 +32,34 @@ def get_boletos():
 
     except Error as e:
        raise HTTPException(status_code=500, detail=f"Database Error : {e}")
+    
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+
+@router.post("/boletos")
+async def post_boletos(boleto: Boleto):
+    if not boleto:
+        raise HTTPException(status_code=400, detail="Body missing or incomplete")
+    
+    conn = get_db_connection()
+    if conn is None:
+        raise HTTPException(status_code=500, detail="Database connection failed")
+    
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO boletos (numero_boleto, data_emissao, data_vencimento, valor, status, cliente, empresa) values (%s,%s,%s,%s,%s,%s,%s)",
+            (boleto.numero_boleto, boleto.data_emissao, boleto.data_vencimento, boleto.valor, boleto.status, boleto.cliente, boleto.empresa)
+        )
+        conn.commit()
+
+        return{"message": "New Item created successfully!"}
+
+    except Error as e:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=f"Database Error : {e}")
     
     finally:
         if conn:
